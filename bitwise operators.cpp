6f16d748d942426910a05682ae1d44fb06749355
Lekha Gubbala <lekhagubbala414@gmail.com>
2025-11-29 11:56:17 +0530

    Add files via upload

---
+++ bitwise operators.cpp
@@ -0,0 +1,13 @@
+//Bitwise operators
+#include<stdio.h>
+int main()
+{
+	int a=6,b=5;
+	printf("%d&%d=%d\n",a,b,a&b);
+	printf("%d|%d=%d\n",a,b,a|b);
+	printf("~%d=%d\n",a,b,~a);
+	printf("%d^%d=%d\n",a,b,a^b);
+	printf("%d<<2=%d\n",a,b,a<<2);
+	printf("%d>>2=%d\n",a,b,a>>2);
+	return 0;
+}
