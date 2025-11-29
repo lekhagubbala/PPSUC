7e45749368aa671568abfc528119ffa32c1041dd
Lekha Gubbala <lekhagubbala414@gmail.com>
2025-11-29 11:55:53 +0530

    Add files via upload

---
+++ biggest of two numbers using if-else.cpp
@@ -0,0 +1,17 @@
+//biggest of two numbers using if-else
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter two numbers:");
+	scanf("%d%d",&a,&b);
+	if(a>b)
+	{
+		printf("%d is greater\n",a);
+	}
+	else
+	{
+		printf("%d is greater\n",b);
+	}
+	return 0;
+}
