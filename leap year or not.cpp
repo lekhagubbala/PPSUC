afd50eaad1b47e7eaacde4f687651b49acf5c0c4
Lekha Gubbala <lekhagubbala414@gmail.com>
2025-11-30 18:05:48 +0530

    Add files via upload

---
+++ leap year or not.cpp
@@ -0,0 +1,23 @@
+// wacp to check the given number is leap year or not
+#include<stdio.h>
+int main()
+{
+	int y;
+	printf("Enter year:");
+	scanf("%d",&y);
+	if(y%4==0)
+	{
+		if(y%100==0)
+		{
+			if(y%400==0)
+				printf("%d is leap year\n",y);
+			else
+			     printf("%d is not a leap year\n",y);
+		}
+		else
+		printf("%d is leap year\n",y);
+	}
+	else
+	printf("%d is not a leap year\n",y);
+	return 0;
+}
