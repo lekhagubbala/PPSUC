83e3976e96d5149c9e935973c098dabcd6e2f626
Lekha Gubbala <lekhagubbala414@gmail.com>
2025-11-30 18:46:13 +0530

    Add files via upload

---
+++ genearte fibonacci series.cpp
@@ -0,0 +1,17 @@
+//generate fibonacci series
+#include<stdio.h>
+int main()
+{
+	int n,i=1,f1=0,f2=1,f3;
+	printf("Enter a number:");
+	scanf("%d",&n);
+	while(i<=n)
+	{
+		printf("%d\t",f1);
+		f3=f1+f2;
+		f1=f2;
+		f2=f3;
+		i++;
+	}
+	return 0;
+}
