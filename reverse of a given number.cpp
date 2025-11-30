221a1f7e007a6ca3b8deb8e1f67d7ce0b24fb3ef
Lekha Gubbala <lekhagubbala414@gmail.com>
2025-11-30 18:54:15 +0530

    Add files via upload

---
+++ reverse of a given number.cpp
@@ -0,0 +1,16 @@
+//reverse of a number
+#include<stdio.h>
+int main()
+{
+	int n,rev,r;
+	printf("Enter a number:");
+	scanf("%d",&n);
+	while(n!=0)
+	{
+		r=n%10;
+		rev=rev*10+r;
+		n=n/10;
+	}
+	printf("Reverse=%d\n",rev);
+	return 0;
+}
