6a5ac6a39d96df7cdfb2762ba85c57d4f849c061
Sowmya Gubbala <lekhagubbala414@gmail.com>
2025-12-06 13:07:04 +0530

    Add files via upload

---
+++ strcmpi function.cpp
@@ -0,0 +1,17 @@
+//strcmpi function
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int res;
+	char s1[]="ABCD";
+	char s2[]="abcd";
+	res=strcmpi(s1,s2);
+	if(res==0)
+	printf("Equal");
+	else if(res<0)
+	printf("Smaller");
+	else
+	printf("Larger");
+	return 0;
+}
