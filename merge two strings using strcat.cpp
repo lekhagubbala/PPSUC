ef11346969a768c7513846f37d3f2b2def7f8ce8
Sowmya Gubbala <lekhagubbala414@gmail.com>
2025-12-06 12:50:32 +0530

    Add files via upload

---
+++ merge two strings using strcat.cpp
@@ -0,0 +1,12 @@
+//merge two strings usings strcat()
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s1[10]="Hello";
+	char s2[10]="World!!";
+	puts(s1);
+	strcat(s1,s2);
+	puts(s1);
+	return 0;
+}
