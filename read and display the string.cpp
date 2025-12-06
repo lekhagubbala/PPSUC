22d33a4fc8f3715dab2d5f9d1b98868b09032a99
Sowmya Gubbala <lekhagubbala414@gmail.com>
2025-12-06 11:19:39 +0530

    Add files via upload

---
+++ read and display the string.cpp
@@ -0,0 +1,12 @@
+//read and display the string
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char name[10];
+	puts("Enter your name:");
+	gets(name);
+	puts("Your name is ");
+	puts(name);
+	return 0;
+}
